e40e16ccaa457c9299bd02c57bce1e2ab7bc9929
iipolovinkin <iipolovinkin@yandex.ru>
2014-09-23 10:43:23 +0400

    add ClientDAO and realise ClientDaoTestImpl

---
+++ src/main/java/ru/blogspot/feomatr/dao/Test/ClientDaoTestImpl.java
@@ -0,0 +1,103 @@
+/**
+ * 
+ */
+package ru.blogspot.feomatr.dao.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ru.blogspot.feomatr.dao.ClientDAO;
+import ru.blogspot.feomatr.entity.Client;
+
+/**
+ * @author igor
+ *
+ */
+public class ClientDaoTestImpl implements ClientDAO {
+	List<Client> clients = new ArrayList<Client>() {
+		{
+			add(new Client(1L, "Alex", "NY", 22));
+			add(new Client(2L, "Nick", "LA", 27));
+			add(new Client(3L, "Ivan", "Spb", 39));
+			add(new Client(4L, "Petr", "RnD", 13));
+		}
+	};
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see ru.blogspot.feomatr.dao.ClientDAO#getAllClients()
+	 */
+	@Override
+	public List<Client> getAllClients() {
+		return clients;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see ru.blogspot.feomatr.dao.ClientDAO#getById(java.lang.Long)
+	 */
+	@Override
+	public Client getById(Long id) {
+		for (Client client : clients) {
+			if (client.getId() == id) {
+				return client;
+			}
+		}
+		return null;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * ru.blogspot.feomatr.dao.ClientDAO#create(ru.blogspot.feomatr.entity.Client
+	 * )
+	 */
+	@Override
+	public Client create(Client cl) {
+		if (cl == null)
+			return cl;
+		Client c = getById(cl.getId());
+		if (c == null)
+			clients.add(cl);
+		return cl;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * ru.blogspot.feomatr.dao.ClientDAO#delete(ru.blogspot.feomatr.entity.Client
+	 * )
+	 */
+	@Override
+	public boolean delete(Client cl) {
+		Client c = getById(cl.getId());
+		if (c == null) {
+			return false;
+		}
+		return clients.remove(c);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * ru.blogspot.feomatr.dao.ClientDAO#update(ru.blogspot.feomatr.entity.Client
+	 * )
+	 */
+	@Override
+	public int update(Client cl) {
+		Client c = getById(cl.getId());
+		if(c==null){
+			return 0;
+		}
+		c.setId(cl.getId());
+		c.setName(cl.getName());
+		c.setAddress(cl.getAddress());
+		c.setAge(cl.getAge());
+		return 1;
+	}
+}
