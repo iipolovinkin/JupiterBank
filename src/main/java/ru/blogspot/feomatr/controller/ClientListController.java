f64ed75ada1b2403628689271f42cd89e897a35f
iipolovinkin <iipolovinkin@yandex.ru>
2014-09-26 03:29:41 +0400

    use DI for ClientServiceImpl and ClientDAOStubImpl

--- src/main/java/ru/blogspot/feomatr/controller/ClientListController.java
+++ src/main/java/ru/blogspot/feomatr/controller/ClientListController.java
@@ -3,7 +3,7 @@
  */
 package ru.blogspot.feomatr.controller;
 
-import java.util.List;
+import javax.inject.Inject;
 import javax.validation.Valid;
 
 import org.slf4j.Logger;
@@ -11,12 +11,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import ru.blogspot.feomatr.dao.stub.ClientDaoStubImpl;
+import ru.blogspot.feomatr.service.ClientService;
 import ru.blogspot.feomatr.entity.Client;
 
 /**
@@ -30,38 +29,62 @@ import ru.blogspot.feomatr.entity.Client;
 public class ClientListController {
 	private static final Logger logger = LoggerFactory
 			.getLogger(ClientListController.class);
+	private ClientService clientService;
+
+	@Inject
+	public ClientListController(ClientService clientService) {
+		this.setClientService(clientService);
+	}
 
 	@RequestMapping(method = RequestMethod.POST)
-	public String addClientFromForm(@Valid Client client, BindingResult bindingResult) {
-		if(bindingResult.hasErrors()){
+	public String addClientFromForm(@Valid Client client,
+			BindingResult bindingResult) {
+		logger.info("addClientFromForm");
+		if (bindingResult.hasErrors()) {
 			return "clients/edit";
 		}
-		
-		//clientService.saveClient(client);
-		
-		return "redirect:/clients/"+client.getId();
+
+		client = getClientService().saveClient(client);
+		logger.info(client.toString());
+
+		return "redirect:/clients/" + client.getId();
 	}
+
 	@RequestMapping(method = RequestMethod.GET, params = "new")
 	public String createClientProfile(Model model) {
+		logger.info("createClientProfile");
 		model.addAttribute(new Client());
 		return "clients/edit";
 	}
 
 	@RequestMapping()
 	public String showCLients(Model model) {
-		logger.info("showCLients");
-		List<Client> clientList = new ClientDaoStubImpl().getAllClients();
-		model.addAttribute("clientList", clientList);
+		logger.info("showClients");
+
+		model.addAttribute("clientList", getClientService().getAllClients());
 		return "clients";
 	}
 
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public String showClient(@PathVariable("id") Long id, Model model) {
 		logger.info("showClient");
-		Client client = new ClientDaoStubImpl().getById(id);
-		//clientService.getClientById(id);
-		model.addAttribute("client", client);
+		model.addAttribute("client", getClientService().getClientById(id));
 		return "clients/show";
+	}
+
+	/**
+	 * @return the clientService
+	 */
+	public ClientService getClientService() {
+		return clientService;
+	}
+
+	/**
+	 * @param clientService
+	 *            the clientService to set
+	 */
+	public void setClientService(ClientService clientService) {
+		this.clientService = clientService;
 	}
 
 }
