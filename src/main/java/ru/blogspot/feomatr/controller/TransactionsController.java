10dbb9cab776a5d1416b61c482d6aa83563c5b38
iipolovinkin <iipolovinkin@yandex.ru>
2014-10-02 22:04:43 +0400

    add filer handler to controller

--- src/main/java/ru/blogspot/feomatr/controller/TransactionsController.java
+++ src/main/java/ru/blogspot/feomatr/controller/TransactionsController.java
@@ -5,12 +5,17 @@ package ru.blogspot.feomatr.controller;
 
 import javax.inject.Inject;
 
+import org.joda.time.DateTime;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import ru.blogspot.feomatr.entity.Account;
+import ru.blogspot.feomatr.entity.Transaction;
+import ru.blogspot.feomatr.formBean.FormFilter;
 import ru.blogspot.feomatr.service.TransactionService;
 
 /**
@@ -31,11 +36,38 @@ public class TransactionsController {
 		this.transactionService = transactionService;
 	}
 
-	@RequestMapping()
-	public String showTransactions(Model model) {
-		logger.info(" {}", "showTransactiosn");
-		model.addAttribute("transactions",
-				transactionService.getAllTransactions());
+	@RequestMapping(method = RequestMethod.POST, params = "clear")
+	public String clearInput(Model model) {
+		logger.info(" {}", "clearInput");
+		model.addAttribute("transactions", transactionService.getAll());
+		model.addAttribute("formFilter", new FormFilter());
 		return "transactions";
 	}
+
+	@RequestMapping()
+	public String showTransactions(Model model) {
+		logger.info(" {}", "showTransactions");
+		model.addAttribute("transactions", transactionService.getAll());
+		model.addAttribute("formFilter", new FormFilter());
+		return "transactions";
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	public String doFilter(FormFilter formFilter, Model model) {
+		logger.info("do Filter");
+		logger.info("FF: {}", formFilter);
+		Account a1, a2;
+		// TransactionFilter = new TransactionFilter();
+		// model.addAttribute("transactionFilter", transactionFilter);
+		// model.addAttribute("formFilter", new FormFilter());
+
+		model.addAttribute("transactions", transactionService.getByFilter(
+				formFilter.getIdFrom(), formFilter.getIdTo(),
+				formFilter.getStartTime(), formFilter.getEndTime()));
+
+		return "transactions";
+	}
+	// class FormFilter {
+
+	// }
 }
