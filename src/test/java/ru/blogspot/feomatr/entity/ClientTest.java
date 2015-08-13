0432d19d9d25b7cb830ccaf08403de593e454d81
iipolovinkin <iipolovinkin@yandex.ru>
2015-08-13 22:56:20 +0300

    JB-30 Use validation constraints in ValidationMessages[fix ClientTest]

--- src/test/java/ru/blogspot/feomatr/entity/ClientTest.java
+++ src/test/java/ru/blogspot/feomatr/entity/ClientTest.java
@@ -1,5 +1,6 @@
 package ru.blogspot.feomatr.entity;
 
+import com.google.common.collect.Sets;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -8,20 +9,35 @@ import javax.validation.ConstraintViolation;
 import javax.validation.Validation;
 import javax.validation.Validator;
 import javax.validation.ValidatorFactory;
+import java.util.Iterator;
 import java.util.Locale;
 import java.util.ResourceBundle;
 import java.util.Set;
 
+import static java.util.Collections.emptySet;
+import static org.fluentjava.FluentUtils.set;
 import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.startsWith;
 import static org.junit.Assert.assertThat;
 
 /**
- * todo use  human readable naming, use AAA, use assertThat
+ * @author iipolovinkin
  */
 public class ClientTest {
     public static ResourceBundle validationMsgs;
     private static Validator validator;
+    private static String clientAgeIsNotNull;
+    private static String clientAgeLessThanValue;
+    private static String clientFirstnameSize;
+    private static String clientAddressSize;
+    private static String clientFirstnamePattern;
+    private static String clientAgeGreaterThanValue;
+    private static int bottomAge = 1;
+    private static int upperAge = 150;
+    private static int clientAddressMinSize = 10;
+    private static int clientAddressMaxSize = 100;
+    private static int clientFirstnameMinSize = 3;
+    private static int clientFirstnameMaxSize = 30;
+
     private Client client;
 
     @BeforeClass
@@ -30,146 +46,176 @@ public class ClientTest {
         validationMsgs = ResourceBundle.getBundle("ValidationMessages");
         ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
         validator = factory.getValidator();
+
+        clientAgeIsNotNull = validationMsgs.getString("client.age.notnull");
+        clientAgeLessThanValue = validationMsgs.getString("client.age.lessThanValue")
+                .replace("{value}", String.valueOf(bottomAge));
+        clientAgeGreaterThanValue = validationMsgs.getString("client.age.greaterThanValue")
+                .replace("{value}", String.valueOf(upperAge));
+        clientFirstnameSize = validationMsgs.getString("client.firstname.size")
+                .replace("{min}", String.valueOf(clientFirstnameMinSize)).replace("{max}", String.valueOf(clientFirstnameMaxSize));
+        clientAddressSize = validationMsgs.getString("client.address.size")
+                .replace("{min}", String.valueOf(clientAddressMinSize)).replace("{max}", String.valueOf(clientAddressMaxSize));
+        clientFirstnamePattern = validationMsgs.getString("client.firstname.pattern");
     }
 
     @Before
     public void setUp() {
-        client = new Client(1l, "Bill", "45 White avenu", 20);
+        long someId = 1L;
+        String validFirstname = "Bill";
+        String validAddress = "10 Downing Street";
+        int validAge = 20;
+        client = new Client(someId, validFirstname, validAddress, validAge);
     }
 
     @Test
-    public void testAgeCanNotBeNull() {
-        String expected = validationMsgs.getString("client.age.notnull");
+    public void testSetNullAgeGetValidationMessage() {
+        Set<String> expected = set(clientAgeIsNotNull);
 
         client.setAge(null);
-        String actual = getConstraintMessage(client);
+        Set<String> actual = getConstraintMessages(client);
 
         assertThat(actual, is(expected));
     }
 
     @Test
-    public void testAgeMustBeGreater0() {
-        String expected = validationMsgs.getString("client.age.lessThanValue")
-                .replace("{value}", "1");
-        int ageLessThanZero = 0;
+    public void testSetAgeLessThanBottomAgeGetValidationMessage() {
+        Set<String> expected = set(clientAgeLessThanValue);
+        int lessThanBottomAge = bottomAge - 1;
 
-        client.setAge(ageLessThanZero);
-        String actual = getConstraintMessage(client);
-
-        assertThat(actual, startsWith(expected));
-    }
-
-    @Test
-    public void testAgeMustBeLess151() {
-        String expected = validationMsgs.getString("client.age.greaterThanValue").replace("{value}", "150");
-        int ageGreaterThan150 = 160;
-
-        client.setAge(ageGreaterThan150);
-        String actual = getConstraintMessage(client);
+        client.setAge(lessThanBottomAge);
+        Set<String> actual = getConstraintMessages(client);
 
         assertThat(actual, is(expected));
     }
 
     @Test
-    public void testValidAge() {
-        String expected = "";
+    public void testSetAgeGreaterThanUpperAgeGetValidationMessage() {
+        Set<String> expected = set(clientAgeGreaterThanValue);
+        int greaterThanUpperAge = upperAge + 1;
+
+        client.setAge(greaterThanUpperAge);
+        Set<String> actual = getConstraintMessages(client);
+
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void testSetValidAge() {
+        Set<String> expected = emptySet();
         int validAge = 10;
 
         client.setAge(validAge);
-        String actual = getConstraintMessage(client);
+        Set<String> actual = getConstraintMessages(client);
 
         assertThat(actual, is(expected));
     }
 
     @Test
-    public void testFirstnameTooShort() {
-        String expected = validationMsgs.getString("client.firstname.size")
-                .replace("{min}", "3").replace("{max}", "30");
+    public void testSetShortFirstnameGetValidationMessage() {
+        Set<String> expected = set(clientFirstnameSize);
         String shortName = "BO";
 
         client.setFirstname(shortName);
-        String actual = getConstraintMessage(client);
+        Set<String> actual = getConstraintMessages(client);
 
         assertThat(actual, is(expected));
     }
 
     @Test
-    public void testFirstnameTooLong() {
-        String expected = validationMsgs.getString("client.firstname.size")
-                .replace("{min}", "3").replace("{max}", "30");
-        String longName = "It_is_very_long_name_for_our_system";
+    public void testSetLpngFirstnameGetValidationMessage() {
+        Set<String> expected = set(clientFirstnameSize);
+        String longName = "ItIsVeryLongNameForOurCurrentSystem";
 
         client.setFirstname(longName);
-        String actual = getConstraintMessage(client);
+        Set<String> actual = getConstraintMessages(client);
 
         assertThat(actual, is(expected));
     }
 
     @Test
-    public void testValidFirstname() {
-        String expected = "";
+    public void testSetValidFirstname() {
+        Set<String> expected = set();
         String validName = "Jake";
 
         client.setFirstname(validName);
-        String actual = getConstraintMessage(client);
+        Set<String> actual = getConstraintMessages(client);
 
         assertThat(actual, is(expected));
     }
 
     @Test
-    public void testFirstnameMustBeAlphabetic() {
-        String expected = validationMsgs.getString("client.firstname.pattern");
+    public void testSetNonAlphabeticFirstnameGetValidationMessages() {
+        Set<String> expected = set(clientFirstnamePattern);
         String nonAlpabeticName = "Bill_M$W|NDOW$";
 
         client.setFirstname(nonAlpabeticName);
-        String actual = getConstraintMessage(client);
+        Set<String> actual = getConstraintMessages(client);
 
         assertThat(actual, is(expected));
     }
 
     @Test
-    public void testAddressTooShort() {
-        String expected = validationMsgs.getString("client.address.size")
-                .replace("{min}", "10").replace("{max}", "100");
+    public void testSetShortAddressGetValidationMessage() {
+        Set<String> expected = set(clientAddressSize);
         String shortAddress = "Address";
 
         client.setAddress(shortAddress);
-        String actual = getConstraintMessage(client);
+        Set<String> actual = getConstraintMessages(client);
 
         assertThat(actual, is(expected));
     }
 
     @Test
-    public void testAddressTooLong() {
-        String expected = validationMsgs.getString("client.address.size")
-                .replace("{min}", "10").replace("{max}", "100");
+    public void testSetLongAddressThenGetValidationMessage() {
+        Set<String> expected = set(clientAddressSize);
         String longString = "AddressTooLongAddressTooLongAddressTooLongAddressTooLongAddressTooLong";
         String longAddress = longString + longString + longString;
 
         client.setAddress(longAddress);
-        String actual = getConstraintMessage(client);
+        Set<String> actual = getConstraintMessages(client);
 
         assertThat(actual, is(expected));
     }
 
 
     @Test
-    public void testValidAddress() {
-        String expected = "";
+    public void testSetValidAddress() {
+        Set<String> expected = emptySet();
         String validAddress = "NY, Wall st. 60";
 
         client.setAddress(validAddress);
-        String actual = getConstraintMessage(client);
+        Set<String> actual = getConstraintMessages(client);
 
         assertThat(actual, is(expected));
     }
 
-    private String getConstraintMessage(Client client) {
+    @Test
+    public void testWhenSetInvalidAgeFirstNameAddressThenGetValidationMessages() {
+        Integer negativeAge = -10;
+        String shortName = "2";
+        String shortAddress = "10st";
+        Set<String> expected = set(clientAgeLessThanValue, clientFirstnameSize, clientAddressSize);
+
+        client.setAge(negativeAge);
+        client.setFirstname(shortName);
+        client.setAddress(shortAddress);
+        Set<String> actual = getConstraintMessages(client);
+
+        assertThat(actual, is(expected));
+    }
+
+    private Set<String> getConstraintMessages(Client client) {
         Set<ConstraintViolation<Client>> constraintViolations = validator.validate(client);
         if (constraintViolations.isEmpty()) {
-            return "";
+            return emptySet();
         } else {
-            return constraintViolations.iterator().next().getMessage();
+            Set<String> messages = Sets.newHashSet();
+            Iterator<ConstraintViolation<Client>> iterator = constraintViolations.iterator();
+            for (; iterator.hasNext(); ) {
+                messages.add(iterator.next().getMessage());
+            }
+            return messages;
         }
     }
 }
