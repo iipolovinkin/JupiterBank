0b26d659fe48cb196acc895ca7551f054a669293
iipolovinkin <iipolovinkin@yandex.ru>
2015-08-28 15:04:54 +0300

    JB-35 Create Initializer for injecting test data into database

--- src/main/java/ru/blogspot/feomatr/dao/hibernate/InitTestData.java
+++ src/main/java/ru/blogspot/feomatr/dao/hibernate/InitTestData.java
@@ -1,85 +1,79 @@
 package ru.blogspot.feomatr.dao.hibernate;
 
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.joda.time.DateTime;
 import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.InitializingBean;
 import ru.blogspot.feomatr.entity.Account;
+import ru.blogspot.feomatr.entity.Broker;
 import ru.blogspot.feomatr.entity.Client;
-import ru.blogspot.feomatr.entity.Transaction;
-import ru.blogspot.feomatr.persistence.hibernate.util.HibernateUtil;
+import ru.blogspot.feomatr.service.AccountService;
+import ru.blogspot.feomatr.service.ClientService;
+import ru.blogspot.feomatr.service.TransferService;
 
 import java.math.BigDecimal;
 
 /**
+ * Inject test data into database before initializing.
+ * Checked on H2, but not checked in case when date already exists in database.
+ * if we use MySQL (not in memory DBRMS);
+ *
  * @author iipolovinkin
  */
-public final class InitTestData {
+public class InitTestData implements InitializingBean {
+    private static final Logger log = LoggerFactory.getLogger(InitTestData.class);
+    public static final int CLIENT_COUNT = 7;
 
-    private static SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
-    private static Client[] clients = new Client[7];
-    private static Account[] accounts;
-    private static Transaction[] transactions;
+    private ClientService clientService;
+    private AccountService accountService;
+    private TransferService transferService;
 
-    private static final String[] addresses = {"New York, Yellow st, 64",
+    private final String[] addresses = {"New York, Yellow st, 64",
             "Washington, Black st, 77", "Minesote, White st, 12",
             "Dallas, Red square, 1", "Springfield, Simpson st, 1",
             "Springfield, Simpson st, 2", "Springfield, Simpson st, 3"};
-    private static final String[] names = {"John", "John2", "John3", "Lisa", "Bart", "Homer", "Marge"};
-    private static final Integer[] ages = {21, 22, 25, 12, 14, 35, 34};
-    private static final String[] dates = {"2014/01/10", "2014/01/20", "2014/02/15", "2014/03/11",
-            "2014/03/22", "2014/04/07", "2014/07/07"};
+    private final String[] names = {"John", "John2", "John3", "Lisa", "Bart", "Homer", "Marge"};
+    private final Integer[] ages = {21, 22, 25, 12, 14, 35, 34};
+    private final String[] dates = {"10.01.14", "20.01.14", "15.02.14", "11.03.14", "22.03.14", "07.04.14", "07.07.14"};
 
-    static void initTransactions() {
-        DateTimeFormatter formatter = DateTimeFormat.forPattern("yyyy/MM/dd");
-        DateTime[] dates = new DateTime[7];
-        for (int i = 0; i < dates.length; ++i) {
-            dates[i] = formatter.parseDateTime(InitTestData.dates[i]);
-        }
-        transactions = new Transaction[7];
-        Session s = sessionFactory.getCurrentSession();
-        s.beginTransaction();
-        for (int i = 0; i < transactions.length; ++i) {
-            BigDecimal bd10 = new BigDecimal(10);
-            transactions[i] = new Transaction(bd10.multiply(new BigDecimal(i)), accounts[0 + i],
-                    accounts[1 + i], dates[i]);
-            s.save(transactions[i]);
-        }
-        s.getTransaction().commit();
-    }
-
-    static void initAccounts() {
-        accounts = new Account[]{new Account(clients[0], 500L),
-                new Account(clients[0], 140L), new Account(clients[1], 140L),
-                new Account(clients[1], 140L), new Account(clients[2], 670L),
-                new Account(clients[2], 1400L), new Account(clients[3], 10L),
-                new Account(clients[4], 1040L)};
-        Session s = sessionFactory.getCurrentSession();
-        s.beginTransaction();
-        for (int i = 0; i < accounts.length; ++i) {
-            s.save(accounts[i]);
-        }
-        s.getTransaction().commit();
-    }
-
-    static void initClients() {
-        Session s = sessionFactory.getCurrentSession();
-        s.beginTransaction();
-        for (int i = 0; i < clients.length; ++i) {
-            clients[i] = new Client(names[i], addresses[i], ages[i]);
-            s.save(clients[i]);
-        }
-        s.getTransaction().commit();
-    }
-
-    public static void initTestData() {
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        log.info("InitTestData initializing ");
         initClients();
-        initAccounts();
         initTransactions();
     }
 
-    public static void setSessionFactory(SessionFactory sessionFactory) {
-        InitTestData.sessionFactory = sessionFactory;
+    private void initTransactions() {
+        DateTimeFormatter formatter = DateTimeFormat.forPattern("dd.MM.yy");
+        for (int i = 0; i < CLIENT_COUNT * 2 - 1; ++i) {
+            transferService.transferTo(new Broker(1l + i, 1l + i, new BigDecimal(i * 10), dates[0]));
+        }
+
+        for (int i = 0; i < CLIENT_COUNT * 2 - 1; ++i) {
+            transferService.transfer(new Broker(0l + i, 1l + i, new BigDecimal(i * 2), dates[0]));
+        }
+    }
+
+    private void initClients() {
+        for (int i = 0; i < CLIENT_COUNT; ++i) {
+            Client client = new Client(names[i], addresses[i], ages[i]);
+            clientService.saveClient(client);
+            accountService.saveAccount(new Account(client));
+            accountService.saveAccount(new Account(client));
+            log.info("# " + i + " clients = " + client);
+        }
+    }
+
+    public void setClientService(ClientService clientService) {
+        this.clientService = clientService;
+    }
+
+    public void setAccountService(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    public void setTransferService(TransferService transferService) {
+        this.transferService = transferService;
     }
 }
--- src/main/java/ru/blogspot/feomatr/entity/Broker.java
+++ src/main/java/ru/blogspot/feomatr/entity/Broker.java
@@ -1,6 +1,7 @@
 package ru.blogspot.feomatr.entity;
 
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -15,6 +16,7 @@ import java.math.BigDecimal;
 @Setter
 @ToString
 @NoArgsConstructor
+@AllArgsConstructor
 public class Broker {
 
     Long accountFrom;
