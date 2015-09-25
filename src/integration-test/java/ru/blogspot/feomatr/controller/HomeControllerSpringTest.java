c65b7af490a546e0269ae53d83f1c0c7e1023331
iipolovinkin <iipolovinkin@yandex.ru>
2015-09-25 07:52:49 +0300

    JB-46 Create Integration Test for HomeController. Use Spring Configuration.(Without Database)

---
+++ src/integration-test/java/ru/blogspot/feomatr/controller/HomeControllerSpringTest.java
@@ -0,0 +1,77 @@
+package ru.blogspot.feomatr.controller;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import javax.annotation.Resource;
+
+/**
+ * @author iipolovinkin
+ * @since 24.09.2015
+ */
+@WebAppConfiguration
+@ContextConfiguration(locations = {"classpath:HomeControllerSpringTest.xml"})
+@RunWith(SpringJUnit4ClassRunner.class)
+public class HomeControllerSpringTest {
+
+    @Resource
+    private WebApplicationContext webApplicationContext;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setUp() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
+    @Test
+    public void testShowHome() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.model().size(0))
+                .andExpect(MockMvcResultMatchers.view().name("home"));
+    }
+
+    @Test
+    public void testShowHomeHome() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/home"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.model().size(0))
+                .andExpect(MockMvcResultMatchers.view().name("home"));
+    }
+
+    @Test
+    public void testShowHomeLogin() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/login"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.model().size(0))
+                .andExpect(MockMvcResultMatchers.view().name("login"));
+    }
+
+    @Test
+    public void testShowAdminPage() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/admin_page"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.model().size(1))
+                .andExpect(MockMvcResultMatchers.model().attributeExists("adminClass"))
+                .andExpect(MockMvcResultMatchers.view().name("admin_page"));
+    }
+
+    @Test
+    public void testDoGenerate() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post("/admin_page"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.model().size(1))
+                .andExpect(MockMvcResultMatchers.model().attributeExists("adminClass"))
+                .andExpect(MockMvcResultMatchers.view().name("admin_page"));
+    }
+}
