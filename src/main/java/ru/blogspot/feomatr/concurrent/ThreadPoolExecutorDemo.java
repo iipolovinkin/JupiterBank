b04e624f1c1803d0c774451615b5864fc875bb4b
sbt-polovinkin-ii <iipolovinkin@yandex.ru>
2019-12-17 18:21:16 +0300

    concurrent1

---
+++ src/main/java/ru/blogspot/feomatr/concurrent/ThreadPoolExecutorDemo.java
@@ -0,0 +1,31 @@
+package ru.blogspot.feomatr.concurrent;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+public class ThreadPoolExecutorDemo {
+    public static void main(String[] args) {
+        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(3, 5, 10_000, TimeUnit.SECONDS, new ArrayBlockingQueue<Runnable>(10));
+
+        for (int i = 0; i < 10; i++) {
+            threadPoolExecutor.submit(createRunnable(i));
+        }
+
+        threadPoolExecutor.shutdown();
+    }
+
+    private static Runnable createRunnable(final Integer num) {
+        return new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    TimeUnit.SECONDS.sleep(4);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("finish num = " + num);
+            }
+        };
+    }
+}
